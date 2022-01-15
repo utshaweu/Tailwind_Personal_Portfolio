cc34e1135f6359728f2546e85e03bad106c07ca7
utshaweu <utshaw1995@gmail.com>
2022-01-15 12:41:32 +0600

    all done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "tailwindcss/tailwind.css";
+import About from './components/About/About';
+import Banner from './components/Banner/Banner';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Work from './components/Work/Work';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navbar/>
+      <Banner/>
+      <About/>
+      <Work/>
+      <Contact/>
+      <Footer/>
+    </main>
   );
 }
 
